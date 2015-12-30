c561ce6e5b1b3070904d384326fbdee32daca11c
Lucas Åström <lucasas@kth.se>
2015-12-30 08:01:09 +0100

    used test convenience functions everywhere

--- test/GameSpec.hs
+++ test/GameSpec.hs
@@ -26,63 +26,53 @@ spec = do
                     addPlayers 2
                     setDeck 1 (replicate 6 "Murloc Raider")
                     setDeck 2 (replicate 6 "Murloc Raider")
-            length (game^.players.ix 0.hand) `shouldBe` 4
-            length (game^.players.ix 1.hand) `shouldBe` 4
-            length (game^.players.ix 0.deck.cards) `shouldBe` 2
-            length (game^.players.ix 1.deck.cards) `shouldBe` 2
+            length (game^?!p1.hand) `shouldBe` 4
+            length (game^?!p2.hand) `shouldBe` 4
+            length (game^?!p1.deck.cards) `shouldBe` 2
+            length (game^?!p2.deck.cards) `shouldBe` 2
             let game2 = play game $ replicateM 4 endTurn
-            length (game2^.players.ix 0.hand) `shouldBe` 6
-            length (game2^.players.ix 1.hand) `shouldBe` 6
-            length (game2^.players.ix 0.deck.cards) `shouldBe` 0
-            length (game2^.players.ix 1.deck.cards) `shouldBe` 0
+            length (game2^?!p1.hand) `shouldBe` 6
+            length (game2^?!p2.hand) `shouldBe` 6
+            length (game2^?!p1.deck.cards) `shouldBe` 0
+            length (game2^?!p2.deck.cards) `shouldBe` 0
 
         it "should damage heroes when drawing from empty deck" $ do
             let game = buildGame $ addPlayers 2
-            length (game^.players.ix 0.deck.cards) `shouldBe` 0
-            length (game^.players.ix 1.deck.cards) `shouldBe` 0
-            game^?!players.ix 0.hero.health `shouldBe` 20
-            game^?!players.ix 1.hero.health `shouldBe` 20
+            length (game^?!p1.deck.cards) `shouldBe` 0
+            length (game^?!p2.deck.cards) `shouldBe` 0
+            game^?!p1.hero.health `shouldBe` 20
+            game^?!p2.hero.health `shouldBe` 20
             let game2 = play game $ replicateM 6 endTurn
-            game2^?!players.ix 0.hero.health `shouldBe` 2
-            game2^?!players.ix 1.hero.health `shouldBe` 2
+            game2^?!p1.hero.health `shouldBe` 2
+            game2^?!p2.hero.health `shouldBe` 2
 
     describe "mana" $ do
         it "should start with correct mana" $ do
             let game = buildGame $ addPlayers 2
-            game^?!players.ix 0.hero.mana `shouldBe` 1
-            game^?!players.ix 0.hero.maxMana `shouldBe` 1
-            game^?!players.ix 1.hero.mana `shouldBe` 0
-            game^?!players.ix 1.hero.maxMana `shouldBe` 0
+            game^?!p1.hero.mana `shouldBe` 1
+            game^?!p1.hero.maxMana `shouldBe` 1
+            game^?!p2.hero.mana `shouldBe` 0
+            game^?!p2.hero.maxMana `shouldBe` 0
 
     describe "attack" $ do
         it "should be able to attack hero with minions" $ do
             let game = buildGame $ do
                     addPlayers 2
                     setActiveMinions 1 ["Murloc Raider"]
-            let p1 = game^?!players.ix 0
-            let p2 = game^?!players.ix 1
-            p1^.hero.health `shouldBe` 20
-            let game2 = play game $ attack p1 (p1^?!activeMinions.ix 0.uuid) (p2^.hero.uuid)
-            game2^?!players.ix 1.hero.health `shouldBe` 18
+            game^?!p1.hero.health `shouldBe` 20
+            let game2 = play game $ attack (game^?!p1) (game^?!p1.m 0.uuid) (game^?!p2.hero.uuid)
+            game2^?!p2.hero.health `shouldBe` 18
 
         it "can only attack on player's turn" $ do
             let game = buildGame $ do
                     addPlayers 2
                     setActiveMinions 1 ["Murloc Raider"]
                     setActiveMinions 2 ["Murloc Raider"]
-            let p1 = game^?!players.ix 0
-            let p2 = game^?!players.ix 1
-            let p1m = p1^?!activeMinions.ix 0
-            let p2m = p2^?!activeMinions.ix 0
-            evalState (canAttack p1 p1m) game `shouldBe` True
-            evalState (canAttack p2 p2m) game `shouldBe` False
+            evalState (canAttack (game^?!p1) (game^?!p1.m 0)) game `shouldBe` True
+            evalState (canAttack (game^?!p2) (game^?!p2.m 0)) game `shouldBe` False
             let game2 = play game endTurn
-            let p1' = game2^?!players.ix 0
-            let p2' = game2^?!players.ix 1
-            let p1m' = p1'^?!activeMinions.ix 0
-            let p2m' = p2'^?!activeMinions.ix 0
-            evalState (canAttack p1' p1m') game2 `shouldBe` False
-            evalState (canAttack p2' p2m') game2 `shouldBe` True
+            evalState (canAttack (game2^?!p1) (game2^?!p1.m 0)) game2 `shouldBe` False
+            evalState (canAttack (game2^?!p2) (game2^?!p2.m 0)) game2 `shouldBe` True
 
         it "can only attack once per minion per turn" $ do
             let game = buildGame $ do
